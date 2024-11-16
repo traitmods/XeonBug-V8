688584da14eeebe26d33ee8607af2367fdd7fc60
traitmods <112393469+traitmods@users.noreply.github.com>
2024-11-16 11:38:19 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,19 +10,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2349155556447"
+global.ownername = "Pheonix"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2349155556447'  //creator number
+global.ownername = 'Pheonix' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'GlobalTech\n\nContact: +923444844060'
+global.author = 'GlobalTech\n\nContact: +2349155556447'
 
 //console view/theme
 global.themeemoji = '🪀'
